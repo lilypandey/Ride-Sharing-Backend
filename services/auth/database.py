8414d2d3c89df344466823b24c6f4f8ed1e79d63
Lily <panlily2002@gmail.com>
2025-09-11 18:11:40 +0530

    Update database.py

--- services/auth/database.py
+++ services/auth/database.py
@@ -1,9 +1,11 @@
+from dotenv import load_dotenv
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/postgres")
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
