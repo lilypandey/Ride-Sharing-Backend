40c504551fbaba488dc38cab9bd84a5c110ce5a1
Lily <panlily2002@gmail.com>
2025-09-11 23:34:02 +0530

    Update redis_client.py

--- services/driver_location/redis_client.py
+++ services/driver_location/redis_client.py
@@ -3,9 +3,11 @@ from fastapi import Depends
 
 REDIS_URL = "redis://redis:6379"
 
+# Creating global Redis pool once
+redis_client = aioredis.from_url(REDIS_URL, decode_responses=True)
+
 async def get_redis():
-    redis = await aioredis.from_url(REDIS_URL, decode_responses=True)
     try:
-        yield redis
+        yield redis_client
     finally:
-        await redis.close()
+        pass
